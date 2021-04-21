d369b34e287998ae555fc9dd64a196491ed48a38
WalidKab <walid.kabli@le-campus-numerique.fr>
2021-04-21 08:54:23 +0200

    Exo 7 OK=>Async/await

--- 7-async-await/script.js
+++ 7-async-await/script.js
@@ -16,3 +16,36 @@ async function main() {
 console.log("= a = BEFORE main call");
 main();
 console.log("= b = AFTER main call");
+
+async function getSpecies(species) {
+  const fetchHuman = await fetch("https://swapi.dev/api/species/"+ species +"/");
+  const humanOk = await fetchHuman.json()
+
+  console.log(humanOk)
+  return humanOk
+}
+
+getSpecies(1);
+
+async function getPeople(specie,char){ 
+  const findSpecies = await getSpecies(specie);
+  const findPeople = await fetch(findSpecies.people[char]);
+  const peopleOk = await findPeople.json();
+
+  console.log(peopleOk)
+}
+
+getPeople(1,3)
+
+async function getAllPeople(specie) {
+  const findSpecies = await getSpecies(specie);
+  const findPeople = await findSpecies.people
+  .map(async displayName => {
+    const fetchName = await fetch(displayName);
+    const nameFind = await fetchName.json()
+    console.log(nameFind.name)
+  })
+
+  console.log(findPeople)
+}
+getAllPeople(1)
